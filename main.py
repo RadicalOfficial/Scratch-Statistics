ec1bfdd40948bb9b899f1b394ad77e319867c031
Knightbot63 <125277461+RadicalOfficial@users.noreply.github.com>
2024-02-08 13:12:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Keep Running
 from s import run
-from flask import Flask
+from flask import Flask, jsonify
 from threading import Thread
 
 app = Flask('')
@@ -10,6 +10,6 @@ t.start()
 
 @app.route('/')
 def home():
-    return "Online."
+    return jsonify({"detail": "Code is running."})
 
 app.run(host='0.0.0.0',port=8080)
